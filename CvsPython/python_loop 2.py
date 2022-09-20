ee040793902fda5a80550c12b952adf221b3a99d
Edward Brash <brash@jlab.org>
2022-09-19 20:25:55 -0400

    Updates

--- CvsPython/python_loop 2.py
+++
@@ -1,6 +0,0 @@
-import sys
-NUMBER = int(sys.argv[1])*1000000
-print("Python s: %d" % NUMBER);
-s = 0
-for i in range(NUMBER):
-	s+=1
--- CvsPython/setup_cpython 2.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-	ext_modules = cythonize("*.pyx")
-)
