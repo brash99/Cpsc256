fff004f993c0d2fb918c1d452d7e929076b0e137
brash99 <brash@jlab.org>
2023-07-08 10:19:12 -0400

    CLion Updates

---
+++ CLionProjects/HelloWorld/main.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.c
+ * Author: brash
+ *
+ * Created on July 26, 2021, 7:21 PM
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/************************************************************/
+/*                                                          */
+/* The simplest program, which prints out "Hello World!!!"  */
+/*                                                          */
+/************************************************************/
+
+int main(int argc, char** argv) {
+    
+    printf("Hello World!!!!\n");
+    
+    return (EXIT_SUCCESS);
+}
+
